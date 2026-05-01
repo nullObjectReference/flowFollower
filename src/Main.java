bdb78a6c821417516ef0bcaf364c830a77e807be
lincoln12833 <Lincolnbruning@protonmail.com>
2026-04-30 22:16:54 -0400

    final submitted

--- src/Main.java
+++ src/Main.java
@@ -8,15 +8,38 @@ public class Main {
     public Robot robot;
     public boolean done = false;
     public Scanner scan = new Scanner(System.in);
-    public static int[] target = new int[]{-4, 5};
+
+    public static int[] target = new int[]{-5, 6};
+    public int moves = 0;
 
     public void main(String[] args) throws IOException {
         flowField = new FlowField(op);
-        robot = new Robot(new Position(3, -3), op);
+        robot = new Robot(new Position(-4, 2), op);
+
+        System.out.println("Enter the target x (a whole number between -5 and 6)");
+        try {
+            target[0] = scan.nextInt();
+            if(target[1] < -5 || target[1] > 6){
+                throw new RuntimeException("out of bounds");
+            }
+        } catch (Exception e){
+            System.out.println("Whoops that's an invalid input, I'll default to -5 (the left edge of the screen) for you");
+        }
+
+        System.out.println("Enter the target y (a whole number between -5 and 6)");
+        try {
+            target[1] = scan.nextInt();
+            if(target[1] < -5 || target[1] > 6){
+                throw new RuntimeException("out of bounds");
+            }
+        } catch (Exception e){
+            System.out.println("Whoops that's an invalid input, I'll default to -5 (the left edge of the screen) for you");
+        }
+
         flowField.flowField[target[0] + 5][target[1] + 5] = 8; //Mark target position with an 8
 
-
         while (!done) {
+            moves += 1;
             flowField.flowField[Robot.position.x + 5][Robot.position.y + 5] = 5; //Mark previous position with a 5
 
             int vectorEffect = flowField.getVectorEffect();
@@ -28,7 +51,13 @@ public class Main {
             for (int i = 0; i < 12; i++) { //Print full field with robot movement for debugging
                 System.out.println(Arrays.toString(flowField.flowField[i]));
             }
+
+            if (moves > 35){
+                System.out.println("Robot trapped (cannot get closer to target), Ending program...");
+                break;
+            }
         }
+        System.out.println("Finished move!");
     }
 }
 
--- src/Robot.java
+++ src/Robot.java
@@ -15,14 +15,14 @@ public class Robot {
         int[] error = new int[] {Math.max(Math.min(Main.target[0] - position.x, 1), -1) , Math.max(Math.min(Main.target[1] - position.y, 1), -1)};
         System.out.println("error x: " + error[0] + " error y: " + error[1]);
 
-        if (flow > 0 && op.flowField.distance < 3 && Math.abs(error[0]) > 0) { //If the robot is close enough to the flow field that it should be effected
+        if (flow > 0 && op.flowField.distance < 3) { //If the robot is close enough to the flow field that it should be effected
             //^ - No effect if the X direction is fulfilled (to be improved and fixed later)
 
             if(op.flowField.distance < 2) { //If the robot is close enough to the flow field that it should be moved away
                 System.out.println("flowed");
                 position.x += directions[flow][0];
                 position.y += directions[flow][1];
-            } else { //If the robot is close enough to the flow field that it should keep its distance
+            } else{ //If the robot is close enough to the flow field that it should keep its distance
                 System.out.println("sustained distance");
                 position.x +=  Math.max(Math.min(error[0] + directions[flow][0], 1), -1);
                 position.y +=  Math.max(Math.min(error[1] + directions[flow][1], 1), -1);
@@ -32,7 +32,7 @@ public class Robot {
             position.x += (error[0]);
             position.y += error[1];
         }
-        return Math.abs(error[0]) < 1 && Math.abs(error[1]) < 1 && flow == 0;
+        return Math.abs(error[0]) < 1 && Math.abs(error[1]) < 1;
     }
 
 }
